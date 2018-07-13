4cbbbb540191e329eb922c20c7be1b1c9f056246
tash02 <tash.brinkley@gmail.com>
2018-07-13 22:46:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Write your solution here!
-let drivers = ["Milo", "Otis", "Garfield"]
+const drivers = ["Milo", "Otis", "Garfield"]
 let ralph = ["ralph"]
 function destructivelyAppendDriver () {
   drivers = [drivers, ralph];
